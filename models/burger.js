3ce7e5113a69610d28a023b80addc1d454689482
Ghost Ghost <ghost77@Ghosts-MBP.hsd1.ca.comcast.net>
2019-06-19 22:03:48 -0700

    created burger js that will call ORM functions

---
+++ models/burger.js
@@ -0,0 +1,30 @@
+// Import the ORM to create functions into burger.js
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    // Adding new burger
+    insertOne: function(cols, val, cb) {
+        orm.insertOne("burgers",cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    // Updates and change devoured statu
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals,condition, function(res) {
+            cb(res);
+        });
+    },
+    delete: function(condition, cb) {
+        orm.delete("burgers", condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+// Export at the end of burger.js file.
+module.exports = burger;
