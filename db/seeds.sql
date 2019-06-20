fa3eeccafc2d5ccba1fe8fd8cd1147298165152b
Ghost Ghost <ghost77@Ghosts-MBP.hsd1.ca.comcast.net>
2019-06-19 19:42:57 -0700

    created cb folder and packages

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- insert queries to populate the `burgers` table with at least three entries.
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chilli Pepper Monterey Jack Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quaterpound Cheeseburger', false);
