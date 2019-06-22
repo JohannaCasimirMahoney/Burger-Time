3c90c0d0b81d5d88866c0ebc8a43470b807b3589
Ghost Ghost <ghost77@Ghosts-MBP.hsd1.ca.comcast.net>
2019-06-22 16:46:45 -0700

    set handlebars and server listening

--- server.js
+++ server.js
@@ -15,3 +15,16 @@ app.use(express.json());
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
+
+app.engine("handlers", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+     // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+})
