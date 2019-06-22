68c194727fc0832e2b4ddcf6fdd1375f7f50d58f
Ghost Ghost <ghost77@Ghosts-MBP.hsd1.ca.comcast.net>
2019-06-22 16:17:09 -0700

    created post,put, and condition 404

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,3 +16,25 @@ router.get("/", function(req, res) {
         res.render("index", hbsObject);
     });
 });
+
+// This creates a new burger to the db
+
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(results) {
+        res,json({ id: results.insertId });
+    });
+});
+
+router.put("/api/burger/:id", function(req,res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+// Setting the condition if the change is not made to 404 display
+    burger.updateOne({ devoured: req.body.devoured }, condition, function(result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        }else {
+            res.status(200).end();
+        }
+    });
+});
