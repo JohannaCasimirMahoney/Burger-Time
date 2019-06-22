e903a3c163fc691ac3270fd99acda657f45aa51e
Ghost Ghost <ghost77@Ghosts-MBP.hsd1.ca.comcast.net>
2019-06-22 16:28:06 -0700

    created the updateOne and delete the burger from db

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -38,3 +38,22 @@ router.put("/api/burger/:id", function(req,res) {
         }
     });
 });
+
+// This will delete the burger from the db.
+
+router.delete("/api/burgers/:id", function(req,res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+
+    // If no rows were changed, then the ID must not exist, so 404
+    burger.deleteOne(condition, function(result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+
+        }else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
