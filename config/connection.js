6908e3bf650e15f0d15661fac215cdfc81cf54de
Ghost Ghost <ghost77@Ghosts-MBP.hsd1.ca.comcast.net>
2019-06-22 19:10:45 -0700

    new folder

--- config/connection.js
+++ config/connection.js
@@ -1,34 +1,22 @@
-
-    
-// Set up MySQL connection.
-
-//code borrowed from week activity catsapp
+// file, setup the code to connect Node to MySQL.
 var mysql = require("mysql");
 
-var connection;
-
-if(process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else{
-
-    connection = mysql.createConnection({
-    port: 3000,
+var connection = mysql.createConnection({
     host: "localhost",
+    port: 3000,
     user: "root",
-    password: "",
+    password: "password",
     database: "burgers_db"
-  });
-
-}
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
+// Make connection.
+connection.connect(function(err){
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for ORM to use.
 module.exports = connection;
