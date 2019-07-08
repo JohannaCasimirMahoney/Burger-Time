8994bd9dd23da4c056bdf84b51ef521254a1baeb
Ghost Ghost <ghost77@Ghosts-MBP.hsd1.ca.comcast.net>
2019-07-07 21:36:51 -0700

    exchanged bodyparser with express added exphbs

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,47 +1,15 @@
-$(function () {
+$(document).ready(function() {
 
-    $(".create-form").on("submit", function (event) {
+    $(".devour-form").on("submit", function(event) {
         event.preventDefault();
 
-        var newBurger = {
-            burger_name: $("#newburger")
-                .val()
-                .trim(),
-            devoured: 0
-        };
-        $.ajax("/api/burger", {
-            type: "POST",
-            data: newBurger
-        }).then(function () {
-            console.log("Added new burger");
-            location.reload();
-        });
-    });
-
-    $(".eatburger").on("click", function (event) {
-        event.preventDefault();
-
-        var id = $(this).data("id");
-        var devouredState = {
-            devoured: 1
-        };
-        $.ajax("/api/burgers/" + id, {
-            type: "PUT",
-            data: devouredState
-        }).then(function () {
-            console.log("Burger devoured");
-            location.reload();
-        });
-    });
-    $(".trashburger").on("click", function(event) {
-        event.preventDefault();
-
-        var id = $(this).data("id");
-
-        // Send the DELETE request.
+        var burger_id = $(this).children(".burger_id").vak();
+        console.log(burger_id);
         $.ajax({
-            type: "DELETE",
-            url: "/api/burgers/" + id
-        }).then(location.reload());
+            method: "PUT",
+            url: "/burgers/" + burger_id
+        }).then(function(data) {
+            location.reload();
+        });
     });
 });
--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 // Setup the Dependencies and Router` for the app, and export the `router` at the end of your file.
-
 var express = require("express");
-var bodyParser = require("body-parser");
 
 var PORT = process.env.PORT || 8080;
-
 var app = express();
 
 // Static for the app from the "public" directory in app directory
 app.use(express.static("public"));
 
 // Parse appliction body as JSON
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
-// Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them
-var routes = require("./controllers/burgers_controller.js");
+var routes = require("./controllers/burgersController.js");
 
 app.use(routes);
 
@@ -29,3 +25,5 @@ app.listen(PORT, function() {
      // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
 });
+
+
